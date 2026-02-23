e95468d784dd55b15d8c5923b5bcaacbe8620e3b
Prince <prince@Princes-Air.lan>
2026-02-23 11:54:55 +0530

    icon add

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,19 +14,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
+        url: '/plant.svg',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/plant.svg',
   },
 }
 
